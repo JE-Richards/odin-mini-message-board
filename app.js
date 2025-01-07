5ccc3e3305a12b803402c6613b9fbe01ddb12d84
JE-Richards <45516368+JE-Richards@users.noreply.github.com>
2025-01-07 15:06:12 +0000

    feat(error): add error handling
    - Create `CustomNotFoundError` class for 404 errors.
    - Add `error.ejs` view to render error messages.
    - Update `messageRouter` to handle invalid message IDs.
    - Add global handler for undefined paths in `app`.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('node:path');
 const indexRouter = require('./routes/indexRouter');
 const newRouter = require('./routes/newRouter');
 const messageRouter = require('./routes/messageRouter');
+const CustomNotFoundError = require('./errors/CustomNotFoundError');
+const { error } = require('node:console');
 
 const app = express();
 const PORT = 3000;
@@ -27,6 +29,22 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/new', newRouter(messages));
 app.use('/message', messageRouter(messages));
 app.use('/', indexRouter(messages));
+
+// Catch-all route
+app.use((req, res, next) => {
+  next(new CustomNotFoundError(`Page not found for ${req.path}`));
+});
+
+// Error-handling middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const errorMessage = err.message || 'Internal server error';
+
+  res.status(statusCode).render('error', {
+    message: errorMessage,
+    statusCode,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Express app - listening on http://localhost:3000`);
--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,11 +1,21 @@
 const { Router } = require('express');
+const CustomNotFoundError = require('../errors/CustomNotFoundError');
 
 const messageRouter = (messages) => {
   const router = Router();
 
-  router.get('/:messageId', (req, res) =>
-    res.render('message', { message: messages[req.params.messageId] })
-  );
+  router.get('/:messageId', (req, res, next) => {
+    const messageId = parseInt(req.params.messageId);
+    const message = messages[messageId];
+
+    if (!message) {
+      return next(
+        new CustomNotFoundError(`Message with ID ${messageId} not found.`)
+      );
+    }
+
+    res.render('message', { message: message });
+  });
 
   return router;
 };
